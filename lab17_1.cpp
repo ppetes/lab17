72b791595577af9cccca115c61d880e78792d938
ratchapon prangthong <ppete247@gmail.com>
2023-02-10 16:39:13 +0700

    lab17_1 fish

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if(i%M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = (rand()%101)*0.01;
+    }
+}
+
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+int col = 0;
+for(int i = 0;i < N ;i++){
+double x = 0;
+    for(int j = 0;j < M ;j++){
+        x += *(dPtr+col);
+        *(sum1+i) = x;
+        col++;
+    }
+}
+}
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+for(int i = 0;i < N*M ;i++){
+        *(sum2+i%M) = 0;
+    }
+    for(int i = 0;i < N*M ;i++){
+        *(sum2+i%M) += *(dPtr+i);
+    }
+}
+
